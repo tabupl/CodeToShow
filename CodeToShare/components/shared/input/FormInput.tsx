b52d2afe4fdf302923e94a954b79b1a35ef177bb
Adam Waśniewski <tabuplaw@gmail.com>
2019-11-21 14:35:19 +0100

    First and the only commit

---
+++ CodeToShare/components/shared/input/FormInput.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./FormInput.scss";
+import { FormInputProps } from "./types";
+
+const FormInput = ({ label, name, type, onChange, onBlur, value, errors, touched, className }: FormInputProps) => (
+  <div className={`form-input ${className}`}>
+    <p className="form-input_label">
+      {label}
+    </p>
+    <input
+      name={name}
+      type={type}
+      onChange={onChange}
+      onBlur={onBlur}
+      placeholder={label}
+      value={value}
+      className="form-input_input"
+    />
+    <div className={`form-input_validation-marker ${touched && !errors && "valid"}`} />
+    <div className="form-input_error">{errors && touched && errors}</div>
+  </div>
+);
+
+export default FormInput;
